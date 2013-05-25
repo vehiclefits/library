719800ba90c0ef06fbdfc13f765c25a848ae77eb
josh <josh.ribakoff@gmail.com>
2013-05-25 18:45:00 -0400

    Restructured code in CLI tool

--- cli/vehicles-list-import.csv.php
+++ cli/vehicles-list-import.csv.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 /**
  * Vehicle Fits
@@ -22,10 +23,57 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class VF_CLI_VehiclesListImportCSV
+class VF_CLI
 {
-    function execute($file)
+    protected $opt;
+
+    function __construct()
     {
+        $this->bootstrap();
+
+        # Define the command line arguments this tool accepts
+        $this->opt = new Zend_Console_Getopt(array(
+            'file|f=s'    => 'file to import',
+            'config|c=s' => 'PHP config file to initialize with'
+        ));
+
+        $this->requireConfig();
+        $this->injectDb();
+    }
+
+    /* Set up include paths & register autoloader */
+    function bootstrap()
+    {
+        require_once(__DIR__ . '/../bootstrap-tests.php');
+    }
+
+    /* Figure out where we are reading the database configuration from */
+    function requireConfig()
+    {
+        $config = $this->opt->getOption('config');
+        if($config) {
+            require_once($config);
+        } elseif(file_exists(__DIR__.'/config.php')) {
+            require_once(__DIR__ . '/config.php');
+        } else {
+            require_once(__DIR__ . '/config.default.php');
+        }
+    }
+
+    /* Inject a database adapter into VF_Singleton using the configuration from previous step */
+    function injectDb()
+    {
+        VF_Singleton::getInstance()->setReadAdapter(new VF_TestDbAdapter(array(
+            'dbname' => getenv('PHP_VAF_DB_NAME'),
+            'username' => getenv('PHP_VAF_DB_USERNAME'),
+            'password' => getenv('PHP_VAF_DB_PASSWORD')
+        )));
+    }
+
+    function importVehicles()
+    {
+        $file = $this->opt->getOption('file');
+
         $writer = new Zend_Log_Writer_Stream('vehicles-list-import.csv.log');
         $log = new Zend_Log($writer);
 
@@ -36,32 +84,6 @@ class VF_CLI_VehiclesListImportCSV
     }
 }
 
-# Set up include paths & register autoloader
-require_once(__DIR__ . '/../bootstrap-tests.php');
-
-# Define the command line arguments this tool accepts
-$opt = new Zend_Console_Getopt(array(
-    'file|f=s'    => 'file to import',
-    'config|c=s' => 'PHP config file to initialize with'
-));
-
-# Figure out where we are reading the database configuration from (default config, or user's config)
-$config = $opt->getOption('config');
-if($config) {
-    require_once($config);
-} elseif(file_exists(__DIR__.'/config.php')) {
-    require_once(__DIR__ . '/config.php');
-} else {
-    require_once(__DIR__ . '/config.default.php');
-}
-
-# Inject a database adapter into VF_Singleton using the configuration from previous step
-VF_Singleton::getInstance()->setReadAdapter(new VF_TestDbAdapter(array(
-    'dbname' => getenv('PHP_VAF_DB_NAME'),
-    'username' => getenv('PHP_VAF_DB_USERNAME'),
-    'password' => getenv('PHP_VAF_DB_PASSWORD')
-)));
-
 # Read some more command line flags and pass them off to our worker class
-$cli = new VF_CLI_VehiclesListImportCSV();
-$cli->execute($opt->getOption('file'));
+$cli = new VF_CLI();
+$cli->importVehicles();
