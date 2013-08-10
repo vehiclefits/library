04a6a741fae5e8db9685922022087b99674c92a7
josh <josh.ribakoff@gmail.com>
2013-08-10 16:52:57 -0400

    Addded cli tool

---
+++ cli/VFTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Vehicle Fits
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to sales@vehiclefits.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Vehicle Fits to newer
+ * versions in the future. If you wish to customize Vehicle Fits for your
+ * needs please refer to http://www.vehiclefits.com for more information.
+
+ * @copyright  Copyright (c) 2013 Vehicle Fits, llc
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class VFTest extends VF_TestCase
+{
+    function setUp()
+    {
+        VF_Singleton::reset();
+        VF_Singleton::getInstance(true);
+        VF_Singleton::getInstance()->setRequest(new Zend_Controller_Request_Http);
+        $database = new VF_TestDbAdapter(array(
+            'dbname' => VAF_DB_NAME,
+            'username' => VAF_DB_USERNAME,
+            'password' => VAF_DB_PASSWORD
+        ));
+        VF_Singleton::getInstance()->setReadAdapter($database);
+
+        $schemaGenerator = new VF_Schema_Generator();
+        $schemaGenerator->dropExistingTables();
+        $schemaGenerator->execute(array('make','model','year'));
+
+        VF_Schema::reset();
+    }
+
+    function tearDown() {}
+
+    function testShouldShowUsage()
+    {
+        exec(__DIR__.'/vf', $output);
+        $this->assertEquals(1,preg_match('#Usage vf#',$output[0]), 'should show usage if called with no command');
+    }
+
+    function testShouldImportVehicles()
+    {
+        $data = "make,model,year\n";
+        $data .= "Honda,Civic,2000";
+        file_put_contents('test.csv',$data);
+
+        $command = __DIR__.'/vf importvehicles test.csv';
+        passthru($command);
+
+        $exists = $this->vehicleExists(array(
+            'make'=>'Honda',
+            'model'=>'Civic',
+            'year'=>2000
+        ));
+        $this->assertTrue($exists, 'should import vehicles');
+    }
+
+    function testShouldExportVehicles()
+    {
+        $this->createVehicle(array('make'=>'Honda','model'=>'Civic','year'=>2000));
+        $command = __DIR__.'/vf exportvehicles ';
+        exec($command, $output);
+        $this->assertEquals('make,model,year',$output[0],'should export field headers');
+        $this->assertEquals('Honda,Civic,2000',$output[1],'should export vehicle row');
+    }
+
+    function testShouldExportFitments()
+    {
+        $this->insertProduct('sku123');
+
+        $data = "sku,make,model,year\n";
+        $data .= "sku123,Honda,Civic,2000";
+        file_put_contents('test.csv',$data);
+
+        $command = __DIR__.'/vf importfitments  --product-table=test_catalog_product_entity test.csv';
+        passthru($command);
+
+        $command = __DIR__.'/vf exportfitments  --product-table=test_catalog_product_entity';
+        exec($command, $output);
+
+        $this->assertEquals('sku,universal,make,model,year,notes', $output[0], 'should export field header');
+        $this->assertEquals('sku123,0,Honda,Civic,2000,""', $output[1], 'should export values');
+    }
+}
---
+++ cli/schemaTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Vehicle Fits
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to sales@vehiclefits.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Vehicle Fits to newer
+ * versions in the future. If you wish to customize Vehicle Fits for your
+ * needs please refer to http://www.vehiclefits.com for more information.
+
+ * @copyright  Copyright (c) 2013 Vehicle Fits, llc
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class VF_Schema_CLITest extends VF_TestCase
+{
+    function setUp()
+    {
+        VF_Singleton::getInstance()->setReadAdapter(new VF_TestDbAdapter(array(
+            'dbname' => VAF_DB_NAME,
+            'username' => VAF_DB_USERNAME,
+            'password' => VAF_DB_PASSWORD
+        )));
+        $schemaGenerator = new VF_Schema_Generator();
+        $schemaGenerator->dropExistingTables();
+        VF_Schema::$levels = null;
+    }
+
+    function tearDown()
+    {
+
+    }
+
+    /**
+     * @expectedException Zend_Db_Statement_Exception
+     */
+    function testShouldStartWithEmptyDatabase()
+    {
+        $schema = new VF_Schema;
+        $schema->getLevels();
+    }
+
+    function testShouldCreateSchemaOverCommandLine()
+    {
+        $command = __DIR__.'/vf schema --force --levels="year,make,model"';
+        exec($command);
+        $schema = new VF_Schema;
+        $this->assertEquals(array('year','make','model'), $schema->getLevels(), 'should create default schema of MMY');
+    }
+
+    function testShouldCreateLevelTable()
+    {
+        $command = __DIR__.'/vf schema --force --levels="year,make,model"';
+        exec($command);
+        $expectedTable = 'elite_level_1_make';
+        $tables = $this->getReadAdapter()->listTables();
+        $this->assertTrue(in_array($expectedTable, $tables), "master schema should create make with '1' in it's name");
+    }
+
+    function testShouldAddSecondSchema()
+    {
+        $command = __DIR__.'/vf schema --force --levels="year,make,model"';
+        exec($command);
+        $command = __DIR__.'/vf schema --add --levels="foo,bar"';
+        exec($command);
+        $schema = new VF_Schema(2);
+        $this->assertEquals(array('foo','bar'), $schema->getLevels(), 'should create second schema');
+    }
+}
