b7187d3871093ec3076c2b017c4e44fba2f255c4
Kyle Cannon <kyle@dcpowerinc.com>
2013-09-25 12:49:19 -0700

    Refactor classes and methods to use an Abstract Test Case along with an Abstract Singleton. This is used for best practices for magento having it's own extended version of VF_AbstractSingleton and the Vehiclefits Core having a VF_Singleton. Both of these classes extend the AbstractSingleton in the ways that are needed to be isolated from one another. The same was done on the VF_TestCase. VF_TestCase was changed to VF_AbstractTestCase and then VF_TestCase was created again to create the Vehicle Fits Core Test Case that used the correct core singleton (VF_Singleton). PSR-2 Coding was used and ran reformat code on many of the files that were modified.

--- library/VF/SearchTests/ListEntitiesMMYAlnumTest.php
+++ library/VF/SearchTests/ListEntitiesMMYAlnumTest.php
@@ -21,7 +21,7 @@
  * @copyright  Copyright (c) 2013 Vehicle Fits, llc
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class VF_SearchTests_Search_ListEntitiesMMYAlnumTest extends VF_SearchTestCase
+class VF_SearchTests_Search_ListEntitiesMMYAlnumTest extends VF_TestCase
 {
     function testShouldListMakes_WhenNoVehicleIsSelected()
     {
--- library/VF/SearchTests/ListEntitiesYMMTest.php
+++ library/VF/SearchTests/ListEntitiesYMMTest.php
@@ -21,7 +21,7 @@
  * @copyright  Copyright (c) 2013 Vehicle Fits, llc
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class VF_SearchTests_Search_ListEntitiesYMMTest extends VF_SearchTestCase
+class VF_SearchTests_Search_ListEntitiesYMMTest extends VF_TestCase
 {
     const MODEL2 = 'model2';
     
