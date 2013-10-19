60cd496cdbec2d988970c065e29392c751448f83
josh <josh.ribakoff@gmail.com>
2013-10-19 16:21:19 -0400

    removed Magento/ecommerce specific methods from search class. May have to create an ecommerce module like @KyleCannon suggested to me

--- library/VF/Search.php
+++ library/VF/Search.php
@@ -49,187 +49,10 @@ class VF_Search implements VF_Configurable
         $this->_request = $request;
     }
 
-    function getFilter()
-    {
-        if (!$this->filter instanceof Elite_Vaf_Model_Catalog_Category_Filter) {
-            $this->filter = new Elite_Vaf_Model_Catalog_Category_FilterImpl();
-            $this->filter->setConfig($this->getConfig());
-        }
-        return $this->filter;
-    }
-
-    function setFilter(Elite_Vaf_Model_Catalog_Category_Filter $filter)
-    {
-        $this->filter = $filter;
-    }
-
     function getSelected($level)
     {
         $search = new VF_FlexibleSearch($this->getSchema(), $this->getRequest());
         return $search->getValueForSelectedLevel($level);
-    }
-
-    function getSearchPostUrl()
-    {
-        return $this->getUrl('vaf/results');
-    }
-
-    /** Just delegates for testability purposes */
-    function getCategories()
-    {
-        $this->getChooser()->setConfig($this->getConfig());
-        return $this->getChooser()->getFilteredCategories($this->getChooser()->getAllCategories());
-    }
-
-    /** Just delegates for testability purposes */
-    function getFilteredCategories(array $categories)
-    {
-        $this->getChooser()->setConfig($this->getConfig());
-        return $this->getChooser()->getFilteredCategories($categories);
-    }
-
-    protected function isHomepage()
-    {
-        return ('index' == $this->getRequest()->getControllerName() && 'cms' == $this->getRequest()->getRouteName());
-    }
-
-    protected function isCmsPage()
-    {
-        return ('page' == $this->getRequest()->getControllerName() && 'cms' == $this->getRequest()->getRouteName());
-    }
-
-    protected function isProductPage()
-    {
-        if ('product' == $this->getRequest()->getControllerName()) {
-            return true;
-        }
-        return false;
-    }
-
-    protected function isCategoryPage()
-    {
-        if ('category' == $this->getRequest()->getControllerName()) {
-            return true;
-        }
-        return false;
-    }
-
-    protected function isVafPage()
-    {
-        if ('vaf' == $this->getRequest()->getModuleName()) {
-            return true;
-        }
-        return false;
-    }
-
-    protected function isChooseVehiclePage()
-    {
-        if ('choosevehicle' == $this->getRequest()->getActionName()) {
-            return true;
-        }
-        return false;
-    }
-
-    /** @return bool if vaf/categoryOnHomepage == true and it is the homepage, show the category chooser */
-    function showCategoryChooser()
-    {
-        $this->getChooser()->setConfig($this->getConfig());
-        if ($this->isHomepage()) {
-            return $this->getChooser()->showCategoryChooserHomepage();
-        } else {
-            return $this->getChooser()->showCategoryChooserNonHomepage();
-        }
-    }
-
-    /** @return bool if vaf/categoryOnHomepage == true and it is the homepage, show the category chooser */
-    function showAllOptionOnCategoryChooser()
-    {
-        $this->getChooser()->setConfig($this->getConfig());
-        if ($this->isHomepage()) {
-            return $this->getChooser()->showAllOptionHomepage();
-        } else {
-            return $this->getChooser()->showAllOptionAllPages();
-        }
-    }
-
-    function getCategoryChooserAllOptionText()
-    {
-        $setting = $this->getConfig()->categorychooser->allOptionText;
-        if ($setting) {
-            return $setting;
-        }
-        return 'All Categories';
-    }
-
-    function categoryAction($categoryId)
-    {
-        if (!$categoryId) {
-            return;
-        }
-        $categoryIdsHomepage = explode(',', $this->getConfig()->search->categoriesThatSubmitToHomepage);
-        $categoryIdsRefresh = explode(',', $this->getConfig()->search->categoriesThatRefresh);
-        if (in_array($categoryId, $categoryIdsHomepage)) {
-            return $this->url('vaf/product/list');
-        }
-        if (in_array($categoryId, $categoryIdsRefresh)) {
-            return '?';
-        }
-    }
-
-    function action()
-    {
-        if ($this->categoryAction($this->currentCategoryId())) {
-            return $this->categoryAction($this->currentCategoryId());
-        }
-        $submitAction = $this->getConfig()->search->submitAction;
-        $submitOnCategoryAction = $this->getConfig()->search->submitOnCategoryAction;
-        $submitOnProductAction = $this->getConfig()->search->submitOnProductAction;
-        $submitOnHomepageAction = $this->getConfig()->search->submitOnHomepageAction;
-        if ($this->getConfig()->category->disable) {
-            return $this->url('vaf/product/list');
-        }
-        if ($this->isCategoryPage()) {
-            if ($submitOnCategoryAction) {
-                return $submitOnCategoryAction;
-            }
-            return '?';
-        }
-        if ($this->isCmsPage() || ('' != $submitAction && '' == $submitOnProductAction && '' == $submitOnHomepageAction)) {
-            if ('refresh' == $submitAction) {
-                return '?';
-            }
-            if ('homepagesearch' == $submitAction) {
-                return $this->url('vaf/product/list');
-            }
-            return $submitAction;
-        }
-        if ('' == $submitAction && '' == $submitOnProductAction && '' == $submitOnHomepageAction) {
-            return $this->url('vaf/product/list');
-        }
-        if ($this->isProductPage()) {
-            if ('' == $submitOnProductAction && '' != $submitAction) {
-                return $submitAction;
-            }
-            if ('refresh' == $submitOnProductAction) {
-                return '?';
-            }
-            if ('homepagesearch' == $submitOnProductAction) {
-                return $this->url('vaf/product/list');
-            }
-            return $submitOnProductAction;
-        }
-        if ($this->isHomepage()) {
-            if ('' == $submitOnHomepageAction && '' != $submitAction) {
-                return $submitAction;
-            }
-            if ('refresh' == $submitOnHomepageAction) {
-                return '?';
-            }
-            if ('homepagesearch' == $submitOnHomepageAction) {
-                return $this->url('vaf/product/list');
-            }
-            return $submitOnHomepageAction;
-        }
     }
 
     function listEntities($level)
@@ -285,46 +108,6 @@ class VF_Search implements VF_Configurable
         return $levels;
     }
 
-    function currentCategoryId()
-    {
-        return $this->current_category_id;
-    }
-
-    function setCurrentCategoryId($id)
-    {
-        $this->current_category_id = $id;
-    }
-
-    function shouldShow($categoryId)
-    {
-        if ($this->isChooseVehiclePage()) {
-            return false;
-        }
-        if (VF_Singleton::getInstance()->getConfig()->category->disable && !$this->isHomepage() && !$this->isVafPage()) {
-            return false;
-        }
-        if (!$this->getTemplate()) {
-            return false;
-        }
-        if (!$this->categoryEnabled($categoryId)) {
-            return false;
-        }
-        return true;
-    }
-
-    function categoryEnabled($categoryId)
-    {
-        if (!$categoryId) {
-            return true;
-        }
-        return $this->getFilter()->shouldShow($categoryId);
-    }
-
-    function getHeaderText()
-    {
-        return $this->translate('Search By Vehicle');
-    }
-
     function getSubmitText()
     {
         return $this->translate('Search');
@@ -335,12 +118,6 @@ class VF_Search implements VF_Configurable
         $schema = new VF_Schema();
         $schema->setConfig($this->getConfig());
         return $schema->getLevels();
-    }
-
-    /** @return Elite_Vaf_Block_Search_CategoryChooser */
-    function getChooser()
-    {
-        return $this->chooser;
     }
 
     function showClearButton()
