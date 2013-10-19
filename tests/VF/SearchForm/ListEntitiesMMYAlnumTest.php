d2cd58b547d41de5ff22355cd50180ebe9ab82a4
josh <josh.ribakoff@gmail.com>
2013-10-19 17:18:21 -0400

    merged tests for SearchForm into one class

--- tests/VF/SearchForm/ListEntitiesMMYAlnumTest.php
+++ tests/VF/SearchForm/ListEntitiesMMYAlnumTest.php
@@ -25,8 +25,8 @@ class VF_SearchForm_Search_ListEntitiesMMYAlnumTest extends VF_TestCase
     function doSetUp()
     {
     }
-
-    function testShouldListMakes_WhenNoVehicleIsSelected()
+    
+    function test_MMYShouldListMakes_WhenNoVehicleIsSelected()
     {
         $this->switchSchema('make,model,year');
         $vehicle = $this->createMMYWithFitment('Honda', 'Civic', '2000');
@@ -36,7 +36,7 @@ class VF_SearchForm_Search_ListEntitiesMMYAlnumTest extends VF_TestCase
         $this->assertEquals($vehicle->getLevel('make')->getTitle(), $actual[0]->getTitle(), 'should list makes when no vehicle is selected');
     }
 
-    function testShouldNotListModelsBeforeMakeIsSelected()
+    function test_MMYShouldNotListModelsBeforeMakeIsSelected()
     {
         $this->switchSchema('make,model,year');
         $this->createMMYWithFitment('Honda', 'Civic', '2000');
@@ -46,7 +46,7 @@ class VF_SearchForm_Search_ListEntitiesMMYAlnumTest extends VF_TestCase
         $this->assertEquals(array(), $actual, 'should not list models before make is selected');
     }
 
-    function testShouldListModels_WhenVehicleIsSelected()
+    function test_MMYShouldListModels_WhenVehicleIsSelected()
     {
         $this->switchSchema('make,model,year');
         $vehicle = $this->createMMYWithFitment('Honda', 'Civic', '2000');
@@ -55,5 +55,139 @@ class VF_SearchForm_Search_ListEntitiesMMYAlnumTest extends VF_TestCase
         $actual = $search->listEntities('model');
         $this->assertEquals(1, count($actual));
         $this->assertEquals('Civic', $actual[0]->getTitle(), 'should list models when make is selected');
+    }
+
+    /**
+     * Should throw exception when asked to list blank level
+     * @expectedException VF_Level_Exception_InvalidLevel
+     */
+    function test_MMYShouldThrowExceptionWhenAskedToListBlankLevel()
+    {
+        $this->switchSchema('make,model,year');
+        $search = new VF_SearchForm();
+        $search->listEntities('');
+    }
+
+    /**
+     * Should throw exception when asked to list invalid level
+     * @expectedException VF_Level_Exception_InvalidLevel
+     */
+    function test_MMYShouldThrowExceptionWhenAskedToListInvalidLevel()
+    {
+        $this->switchSchema('make,model,year');
+        $search = new VF_SearchForm();
+        $search->listEntities('foo');
+    }
+
+    function test_MMYShouldBeNoModelsPreselected_WhenNoVehicleIsSelected()
+    {
+        $this->switchSchema('make,model,year');
+        $this->createMMYWithFitment('Honda', 'Civic', '2000');
+        $search = new VF_SearchForm();
+        $search->setRequest($this->getRequest());
+        $actual = $search->listEntities('model');
+        $this->assertEquals(array(), $actual, 'should not list models before make is selected');
+    }
+
+    function test_MMYShouldListModels_WhenPartialVehicleIsSelected()
+    {
+        $this->switchSchema('make,model,year');
+        $this->createMMYWithFitment('Honda', 'Civic', '2000');
+        $_GET['make'] = 'Honda';
+        $search = new VF_SearchForm();
+        $search->setRequest($this->getRequest());
+        $actual = $search->listEntities('model');
+        $this->assertEquals(1, count($actual), 'should list models when just make is selected');
+        $this->assertEquals('Civic', $actual[0]->getTitle(), 'should list models when just make is selected');
+    }
+
+    function test_YMMShouldListYearsInUse()
+    {
+        $this->switchSchema('year,make,model');
+        $this->createMMYWithFitment('Honda', 'Civic', '2000');
+        $search = new VF_SearchForm();
+        $search->setRequest($this->getRequest());
+        $actual = $search->listEntities('year', '');
+        $this->assertEquals(1, count($actual));
+        $this->assertEquals('2000', $actual[0]->getTitle(), 'should list years when year not yet selected');
+    }
+
+    function test_YMMShouldListMakesInUse()
+    {
+        $this->switchSchema('year,make,model');
+        $vehicle = $this->createMMYWithFitment('Honda', 'Civic', '2000');
+        $search = new VF_SearchForm();
+        $search->setRequest($this->getRequest());
+        $request = $this->getRequest($vehicle->toTitleArray());
+        $search->setRequest($request);
+        $this->setRequest($request);
+        $actual = $search->listEntities('make');
+        $this->assertEquals(1, count($actual));
+        $this->assertEquals('Honda', $actual[0]->getTitle(), 'should list makes in use when model is selected');
+    }
+
+    function test_YMMShouldNotListMakesNotInUse()
+    {
+        $this->switchSchema('year,make,model');
+        $vehicle = $this->createVehicle(array('make' => 'Honda', 'model' => 'Civic', 'year' => 2000));
+        $search = new VF_SearchForm();
+        $search->setRequest($this->getRequest());
+        $request = $this->getRequest($vehicle->toTitleArray());
+        $search->setRequest($request);
+        $this->setRequest($request);
+        $actual = $search->listEntities('make');
+        $this->assertEquals(0, count($actual), 'should not list makes not in use when model is selected');
+    }
+
+    function test_YMMShouldListYearsNotInUseIfConfigSaysTo()
+    {
+        $this->switchSchema('year,make,model');
+        $config = new Zend_Config(array('search' => array('showAllOptions' => 'true')));
+        $vehicle = $this->createVehicle(array('make' => 'Honda', 'model' => 'Civic', 'year' => 2000));
+        $search = new VF_SearchForm();
+        $search->setConfig($config);
+        $search->setRequest($this->getRequest());
+        $request = $this->getRequest($vehicle->toTitleArray());
+        $search->setRequest($request);
+        $this->setRequest($request);
+        $actual = $search->listEntities('year');
+        $this->assertEquals(1, count($actual), 'should list years not in use when config says to');
+    }
+
+    function test_YMMShouldListMakesNotInUseIfConfigSaysTo()
+    {
+        $this->switchSchema('year,make,model');
+        $config = new Zend_Config(array('search' => array('showAllOptions' => 'true')));
+        $vehicle = $this->createVehicle(array('make' => 'Honda', 'model' => 'Civic', 'year' => 2000));
+        $search = new VF_SearchForm();
+        $search->setConfig($config);
+        $search->setRequest($this->getRequest());
+        $request = $this->getRequest($vehicle->toTitleArray());
+        $search->setRequest($request);
+        $this->setRequest($request);
+        $actual = $search->listEntities('make');
+        $this->assertEquals(1, count($actual), 'should list makes not in use when config says to');
+    }
+
+    function test_YMMListModel()
+    {
+        $this->switchSchema('year,make,model');
+        $vehicle = $this->createMMYWithFitment('Honda', 'Civic', '2000');
+        $search = new VF_SearchForm();
+        $search->setRequest($this->getRequest($vehicle->toTitleArray()));
+        $actual = $search->listEntities('model');
+        $this->assertEquals(1, count($actual));
+        $this->assertEquals('Civic', $actual[0]->getTitle(), 'should list models when make is selected');
+    }
+
+    function testSelected()
+    {
+        $this->switchSchema('make,model,year');
+        $vehicle = $this->createMMY('Honda', 'Civic', '2000');
+        $request = new Zend_Controller_Request_Http;
+        $request->setParams($vehicle->toTitleArray());
+        $search = new VF_SearchForm;
+        $search->setRequest($request);
+        $this->assertEquals($vehicle->getValue('model'), $search->getSelected('model'));
     }
 }
